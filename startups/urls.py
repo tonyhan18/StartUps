1dcad0992ccb95af8cd3486516199993268fc855
tonyhan18 <ourclub7279@gmail.com>
2021-01-15 13:38:18 +0900

    agagin

--- startups/urls.py
+++ startups/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('rhksflwk/', admin.site.urls),
     path('cu/',include("customer.urls")),
 ]
